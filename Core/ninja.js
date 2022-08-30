6ef944937377ac01b0087d236f1ca95538a28a2e
PPurington77 <papurington@gmail.com>
2022-08-29 21:12:10 -0600

    super ninja

--- Core/ninja.js
+++ Core/ninja.js
@@ -24,8 +24,38 @@ class Ninja {
     }
 }
 
-var ninja1 = new Ninja("Patrick", 10);
-ninja1.sayName();
-ninja1.showStats();
-ninja1.drinkSake();
-ninja1.showStats();
+var ninja1 = new Ninja("Patrick");
+// ninja1.sayName();
+// ninja1.showStats();
+// ninja1.drinkSake();
+// ninja1.showStats();
+
+//create sensai class that extends ninja
+//200 health, 10 speead/strength
+//new attribute called wisdom = 10
+//speak wisdom method...calss drinksake() and logs a message
+
+class Sensai extends Ninja {
+    constructor(name) {
+        super(name);
+        this.health = 200;
+        this.strength = 10;
+        this.health = 10;
+        this.wisdom = 10;
+    }
+
+    showTheStats() {
+        console.log(this.health, this.strength, this.health, this.wisdom, this.name);
+    }
+
+    speakWisdoms() {
+        super.drinkSake();
+        console.log(this.health);
+        console.log("Wise Message");
+    }
+}
+
+var sensai = new Sensai("Chungus");
+// sensai.showTheStats();
+sensai.speakWisdoms();
+
