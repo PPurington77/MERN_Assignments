ca2b8332b9deaac4a6706c11cf5475dbdcf7114d
PPurington77 <papurington@gmail.com>
2022-09-01 10:30:08 -0600

    putting it all together

--- Practice/propitup/src/App.js
+++ Practice/propitup/src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <div className="App">
       <PersonCard firstName = { "Patrick" } lastName = { "Purington" } age = { 30 } hairColor = { "Brown" }/>
-      <PersonCard firstName = { "Patrick" } lastName = { "Purington" } age = { 30 } hairColor = { "Brown" }/>
-      <PersonCard firstName = { "Patrick" } lastName = { "Purington" } age = { 30 } hairColor = { "Brown" }/>
-      <PersonCard firstName = { "Patrick" } lastName = { "Purington" } age = { 30 } hairColor = { "Brown" }/>
+      <PersonCard firstName = { "Debby" } lastName = { "Purington" } age = { 52 } hairColor = { "Brown" }/>
+      <PersonCard firstName = { "Erek" } lastName = { "Purington" } age = { 56 } hairColor = { "Brown" }/>
+      <PersonCard firstName = { "Doug" } lastName = { "Purington" } age = { 27 } hairColor = { "Brown" }/>
     </div>
   );
 }
--- Practice/propitup/src/components/PersonCard.js
+++ Practice/propitup/src/components/PersonCard.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentAge : props.age
+        }
+    }
+
+    addYear = () => {
+        this.setState({
+            currentAge : this.state.currentAge + 1
+        })
+    }
+
     render() {
-        const { firstName, lastName, age, hairColor } = this.props;
+        const { firstName, lastName, hairColor } = this.props;
         return (
             <div>
                 <h1>{ lastName }, { firstName }</h1>
-                <h4>{ age }</h4>
+                <h4>{ this.state.currentAge }</h4>
                 <h4>{ hairColor }</h4>
+                <button onClick={this.addYear}>Birthday Button for { firstName } { lastName }</button>
             </div>
         );
     }
