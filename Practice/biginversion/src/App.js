5b7a63f96ed9b9bd71e80dcab093f057dfa9e140
PPurington77 <papurington@gmail.com>
2022-09-01 14:17:56 -0600

    big inversion

---
+++ Practice/biginversion/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard firstName = { "Patrick" } lastName = { "Purington" } age = { 30 } hairColor = { "Brown" }/>
+      <PersonCard firstName = { "Debby" } lastName = { "Purington" } age = { 52 } hairColor = { "Brown" }/>
+      <PersonCard firstName = { "Erek" } lastName = { "Purington" } age = { 56 } hairColor = { "Brown" }/>
+      <PersonCard firstName = { "Doug" } lastName = { "Purington" } age = { 27 } hairColor = { "Brown" }/>
+    </div>
+  );
+}
+
+export default App;
