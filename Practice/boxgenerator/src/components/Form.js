8e7fb3611034a1918609a2c09bcd5f47d0ba19b3
PPurington77 <papurington@gmail.com>
2022-09-05 19:59:31 -0600

    update

--- Practice/boxgenerator/src/components/Form.js
+++ Practice/boxgenerator/src/components/Form.js
@@ -5,13 +5,11 @@ const Form = (props) => {
     const [boxes, setBoxes] = useState([]);
     const [newBoxColor, setNewBoxColor] = useState('');
 
-    //function to handle the state of the form input and update it
     const handleForm = e => {
 
         setNewBoxColor(e.target.value);
     }
 
-    //function to handle the form submission via submit button
     const handleSubmit = e => {
         e.preventDefault();
 
@@ -19,12 +17,8 @@ const Form = (props) => {
             color: newBoxColor
         };
 
-        //call setAll_colors to update all_colors variable to add our new color. setAll_colors needs to be passed a BRAND NEW array of colors
         setBoxes([...boxes, newBox]);
 
-
-        // Ninja Bonus: clear out the color input on successful submission.
-        //to clear the input box after submission, we need to set the input box to an empty string
         setNewBoxColor('');
 
     }
