d3f4f64bb6db18d20b24409692d59ff951eaced1
PPurington77 <papurington@gmail.com>
2022-09-07 15:13:20 -0600

    Fixed Image

--- Core/lukeskywalker-api/src/components/Error.js
+++ Core/lukeskywalker-api/src/components/Error.js
@@ -1,9 +1,9 @@
-
-export const Error = (props) => {
+import obi from './image/obw.jpg'
+export const Error = () => {
     return (
         <div>
             <h1>The are not the droids you are looking for</h1>
-            <img src='./image/obw.jpg' alt='obi wan'/>
+            <img src={ obi } alt='obi wan'/>
         </div>
     )
 }
