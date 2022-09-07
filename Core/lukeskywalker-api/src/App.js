14e01bb7acd4527838b071f4cf55e41cec8c829a
PPurington77 <papurington@gmail.com>
2022-09-07 11:50:10 -0600

    App updated with form

--- Core/lukeskywalker-api/src/App.js
+++ Core/lukeskywalker-api/src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import Form from './components/Form';
 
 function App() {
   return (
     <div>
-      
+      <Form/>
     </div>
   );
 }
