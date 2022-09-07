d1ccdf408a0bfbad3566fdd732e552a85e3d5e1f
PPurington77 <papurington@gmail.com>
2022-09-07 13:23:54 -0600

    Error page added

---
+++ Core/lukeskywalker-api/src/components/Error.js
@@ -0,0 +1,11 @@
+
+export const Error = (props) => {
+    return (
+        <div>
+            <h1>The are not the droids you are looking for</h1>
+            <img src='./image/obw.jpg' alt='obi wan'/>
+        </div>
+    )
+}
+
+export default Error;
