9a5f82e2ae1b37ff8a7593187b0355b8bce8193b
PPurington77 <papurington@gmail.com>
2022-09-13 07:34:35 -0600

    Home Page Complete

--- Core/ProductManager/client/src/App.js
+++ Core/ProductManager/client/src/App.js
@@ -1,7 +1,6 @@
 import { Link, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { AllProducts } from './views/AllProducts';
-import NewProduct from './views/NewProduct';
+import Products from './views/Products';
 
 function App() {
   return (
@@ -9,13 +8,10 @@ function App() {
     <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top justify-content-center mb-4">
       <h1 className="navbar-brand mb-0">Product Creator</h1>
     </nav>
-    < NewProduct/>
     <Routes>
       {/* Redirect Ex */}
       <Route path="/" element={ < Navigate to="/products" replace /> } />
-      <Route path="/products" element={ <AllProducts /> } />
-      {/* <Route path="/jokes/:id/edit" element {<EditJoke /> } /> */}
-      {/* <Route path="/jokes/new" element={ <NewJoke /> } /> */}
+      <Route path="/products" element={ <Products /> } />
     </Routes>
   </div>
   );
