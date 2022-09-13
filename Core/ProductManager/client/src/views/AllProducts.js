3129b7ad7544bd1e16cdddedbd0163c16f82f184
PPurington77 <papurington@gmail.com>
2022-09-12 20:36:45 -0600

    View All Products complete

---
+++ Core/ProductManager/client/src/views/AllProducts.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import { getAllProducts } from '../services/internalApiService';
+
+export const AllProducts = (props) => {
+    const [products, setProducts] = useState([]);
+    
+    useEffect(() => {
+        getAllProducts()
+            .then((data) => {
+                setProducts(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }, [])
+
+    return (
+        <div className="w-50 mx-auto text-center">
+        <h2>All Products</h2>
+        {products.map((product) => {
+            const { _id, title, price, description } = product;
+
+            return <div key={ _id } className="shadow mb-4 rounded border p-4">
+                <h4>{ title }</h4>
+                <h4>{ price }</h4>
+                <h4>{ description }</h4>
+            </div>
+        })}
+    </div>
+    )
+}
+
+export default AllProducts;
