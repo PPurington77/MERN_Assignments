69127b0ee9ed2a49a4c62a2b11835349d8fff0de
PPurington77 <papurington@gmail.com>
2022-09-12 20:43:03 -0600

    Updated

--- Core/ProductManager/client/src/App.js
+++ Core/ProductManager/client/src/App.js
@@ -7,20 +7,6 @@ function App() {
     <div className="container">
     <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top justify-content-center mb-4">
       <h1 className="navbar-brand mb-0">Product Creator</h1>
-      <div className="navbar-nav justify-content-between">
-        <Link
-          to="/products"
-          className="btn btn-sm btn-outline-primary mx-1"
-        >
-          Products
-        </Link>
-        <Link
-          to="/products/new"
-          className="btn btn-sm btn-outline-info mx-1"
-        >
-          New Product
-        </Link>
-      </div>
     </nav>
     <Routes>
       {/* Redirect Ex */}
