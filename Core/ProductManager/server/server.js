9c24488b7b44c889e29bd07f99671e07ae3b301d
PPurington77 <papurington@gmail.com>
2022-09-12 20:10:07 -0600

    server.js done

--- Core/ProductManager/server/server.js
+++ Core/ProductManager/server/server.js
@@ -4,4 +4,16 @@ const port = 8000;
 
 const { productRouter } = require('./routes/product.routes');
 
+require('./config/mongoose.config');
 
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/api/products', productRouter);
+
+app.listen(port, () =>
+    console.log(`Listening on port ${ port } for req and res`)
+);
