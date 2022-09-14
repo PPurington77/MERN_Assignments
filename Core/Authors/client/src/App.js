f8513da8ca145b1a126692ea7fca0d1fb457bb11
PPurington77 <papurington@gmail.com>
2022-09-14 14:34:58 -0600

    App.js updated

--- Core/Authors/client/src/App.js
+++ Core/Authors/client/src/App.js
@@ -1,5 +1,7 @@
-
 import './App.css';
+import { AllAuthors } from './views/AllAuthors';
+import { Navigate, Routes, Route, Link } from 'react-router-dom';
+import { NewAuthor } from './views/NewAuthor';
 
 function App() {
   return (
@@ -7,6 +9,11 @@ function App() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top justify-content-center mb-4">
         <h1 className="navbar-brand mb-0">Favorite Authors</h1>
       </nav>
+      <Routes>
+        <Route path="/" element = { <Navigate to="/authors" replace /> } />
+        <Route path="/authors" element = { <AllAuthors /> } />
+        <Route path="/authors/new" element = { <NewAuthor />} />
+      </Routes>
     </div>
   );
 }
