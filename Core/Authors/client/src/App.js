52e5f1cc4a73b998e1e0e71b4fce561a3dc7532f
PPurington77 <papurington@gmail.com>
2022-09-14 12:05:12 -0600

    App.js updated

--- Core/Authors/client/src/App.js
+++ Core/Authors/client/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top justify-content-center mb-4">
+        <h1 className="navbar-brand mb-0">Favorite Authors</h1>
+      </nav>
     </div>
   );
 }
